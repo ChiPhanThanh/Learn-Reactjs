1df309808ee79e2e7345b89bff55b1f9bea2c1dd
Phan Thanh Chi <chi.phanthanh.ncc@gmail.com>
2022-02-18 17:57:54 +0700

    learn Reactjs - component header and body

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import ComponentOne from './component/ComponentOne';
+import Header from './component/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [
+        {
+          name: "Laptop Asus",
+          price: "10000000",
+          imge: "img/lenovo.png",
+          hot: true
+        },
+
+        {
+          name: "Laptop Dell",
+          price: "20000000",
+          imge: "img/lenovo.png",
+          hot: false
+        },
+
+        {
+          name: "Laptop Acer",
+          price: "167000000",
+          imge: "img/lenovo.png",
+          hot: true
+        },
+
+        {
+          name: "Laptop ThinkPad",
+          price: "34000000",
+          imge: "img/lenovo.png",
+          hot: false
+        },
+
+        {
+          name: "Laptop Huawei",
+          price: "23000000",
+          imge: "img/lenovo.png",
+          hot: true
+        },
+
+        {
+          name: "Laptop Lampad",
+          price: "34000000",
+          imge: "img/lenovo.png",
+          hot: false
+        },
+
+        {
+          name: "Laptop Ronaldo",
+          price: "34000000",
+          imge: "img/lenovo.png",
+          hot: true
+        },
+
+        {
+          name: "Laptop Donaltrum",
+          price: "34000000",
+          imge: "img/lenovo.png",
+          hot: true
+        },
+      ],
+    };
+  }
+
+  show_product = () =>{
+    const listProduct = this.state.products.map((iteams, index) =>
+      <ComponentOne key = {index} price={iteams.price} image={iteams.imge} hot={iteams.hot}>
+        {iteams.name}
+      </ComponentOne>
+    );
+    return listProduct;
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="ComponentOne">
+          {this.show_product()}
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
